886d09591fe429ce1e345c1ef09cfa6f98892eea
UnciniGustavo20 <uncinigustavo149@gmail.com>
2026-03-26 11:48:55 -0300

    atividade1

---
+++ atividades/atividade1.js
@@ -0,0 +1,11 @@
+let produto = "garafa"; 
+let preco = 40.0;
+let estoque = true;
+
+console.log(`o item ${produto} e custa ${preco}`);
+
+if(estoque != false){
+    console.log("produto disponivel");
+}else{
+    console.log("produto indisponivel"); 
+}
