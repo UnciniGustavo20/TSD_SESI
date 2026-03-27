9b318d6695e8636847db844c0b814ca40ee42ebe
UnciniGustavo20 <uncinigustavo149@gmail.com>
2026-03-27 12:01:24 -0300

    atividade6

---
+++ atividades/atividade6.js
@@ -0,0 +1,14 @@
+var aluno = "gustavo"
+var nota1 = 9.0;
+var nota2 = 7.5;
+
+media = (nota1 + nota2)/2;
+if(media >= 7.5){
+console.log("aprovado")
+}else if(media <= 7.4 && media >= 5.0){
+    console.log("recuperacao!")
+}else{
+    console.log("reprovado!")
+}
+
+
