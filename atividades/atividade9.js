c37354947d631592359c4470207d32210d24b24a
UnciniGustavo20 <uncinigustavo149@gmail.com>
2026-04-02 11:06:34 -0300

    atividade9

---
+++ atividades/atividade9.js
@@ -0,0 +1,8 @@
+let hora = 15
+if(hora >= 0 && hora <= 11){
+    console.log("bom dia!");
+}else if(hora >= 12 && hora <= 17){
+    console.log("boa tarde!");
+}else{
+    console.log("boa noite!")
+}
