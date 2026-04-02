b9d4ce3b74b1721ecb6d4ae2a21c7b6aba6809f4
UnciniGustavo20 <uncinigustavo149@gmail.com>
2026-04-02 11:54:16 -0300

    atividade10

---
+++ atividades/atividade10.js
@@ -0,0 +1,11 @@
+const hardware = ["ram", "plcaDeVideo", "processador", "placaMae", "monitor"];
+
+console.log(hardware);
+
+hardware.push("fonte");
+
+console.log(hardware);
+
+hardware.shift(0);
+
+console.log(hardware);
