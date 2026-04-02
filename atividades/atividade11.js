9b02dc0e1d36362b6d9767c97d909dc18753cafd
UnciniGustavo20 <uncinigustavo149@gmail.com>
2026-04-02 12:03:48 -0300

    atividade11

---
+++ atividades/atividade11.js
@@ -0,0 +1,8 @@
+const nomes = ["Gustavo", "Miguel", "Motta", "rafael", "Igor", "pedro", "Bruno", "Alex", "Emanuel", "Lucas"];
+
+if(nomes.includes("Gustavo")==true){
+    console.log("usuário encontrado no sistema! ");
+}
+else{
+    console.log("usuário não localizado");
+}
